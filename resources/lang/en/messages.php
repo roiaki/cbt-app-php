4d498a41e0b3245fe285192634cfc198c662a134
roiaki <roitoitoitoi@yahoo.co.jp>
2022-06-04 17:09:50 +0900

    #9 説明ページの多言語化

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -8,4 +8,16 @@ return [
     'sentence01' => 'Grasp your thinking habits in columns 3 and 7',
     'sentence02' => 'Lets get rid of cognitive distortion',
     'register' => 'Register',
+
+    // 説明ページ
+    'info_title' => 'How to use this app',
+    'step01' => 'STEP1 Grasp the event',
+    'step02' => 'STEP2 3 columns',
+    'step03' => 'STEP3 7 columns',
+
+    'info_sentence01' => 'Recall and describe events that are depressed or have negative thoughts.',
+    'info_sentence02' => 'Please remember and describe the events that I want to change my way of thinking in a negative way. Write down how you thought and felt in three columns. Also, check if there are any unpleasant habits in the process of thinking.',
+    'info_sentence03' => 'Please click the "7 Column Creation Button" from the 3 column details page to enter. Verify based on objective facts and modify to a balanced and favorable cognition.',
+    
+    
 ];
--- resources/views/users/info.blade.php
+++ resources/views/users/info.blade.php
@@ -4,23 +4,21 @@
 
 <div class="row justify-content-center">
   <div class="col-sm-8">
-    <h3 class="title_head">このアプリの使い方</h3>
+    <h3 class="title_head">{{ __('messages.info_title') }}</h3>
     <br>
-    <p><span class="under_line">STEP1　出来事把握</span><br>
-      まず、落ち込んでしまったり<br>
-      ネガティブで考え方を変えたい出来事を思い出し記載します。<br>
+    <p>
+      <span class="under_line">{{ __('messages.step01') }}</span><br>
+      {{ __('messages.info_sentence01') }}
       <br>
-      <span class="under_line">STEP2　3コラム</span><br>
-      その出来事を元にどのように考え、感じたのかを3コラムに分けて記入しましょう。<br>
-      また、その考えに好ましくない癖がないか確認してみましょう。
+    </p>
+    <p>
+      <span class="under_line">{{ __('messages.step02') }}</span><br>
+      {{ __('messages.info_sentence02') }}  
     </p>
 
-    <p><span class="under_line">STEP3　7コラム</span><br>
-      次に３コラム詳細ページから７コラムの作成ボタンをクリックして<br>
-      7コラムに分けて、考えた過程を記載しましょう。<br>
-      <br>
-      客観的事実に基づいて検証し、<br>
-      自分にとって好ましい認知へ修正しましょう。
+    <p><span class="under_line">{{ __('messages.step03') }}</span><br>
+      {{ __('messages.info_sentence03') }}<br>
+      
     </p>
   </div>
 </div>
