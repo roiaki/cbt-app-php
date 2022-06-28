4c32428e1a82d3d0a0859c100690776da2617a41
roiaki <roitoitoitoi@yahoo.co.jp>
2022-06-28 16:14:29 +0900

    model作成

---
+++ database/migrations/2022_06_28_153328_create_solutions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSolutionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('solutions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('trouble', 500);
+            $table->string('solution', 500);
+            $table->string('merit', 500);
+            $table->string('demerit', 500);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('solutions');
+    }
+}
