06da6ce0e8513be897da89e5f796e21a4d8f944e
roiaki <roitoitoitoi@yahoo.co.jp>
2022-07-03 17:57:48 +0900

    色変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,18 @@
     <div class="jumbotron" style="background:url(images/welcome_image.png); background-size:cover;">
       <div class="text-center">
         <p >{{ __('messages.sentence01') }}<br>{{ __('messages.sentence02') }}</p>
-        <h1 class="outline">{{ __('messages.welcome_title') }}</h1>
-        <h3 class="message animate__animated animate__zoomInDown">May you have many Smiles!</h3>
+        <h1 class="outline">{{ __('messages.welcome_title') }}</h1><br>
+        <div>
+          <h3>
+            <span class="message animate__animated animate__fadeIn animate__delay-1s">May</span>
+            <span class="message animate__animated animate__fadeIn animate__delay-2s">you</span>
+            <span class="message animate__animated animate__fadeIn animate__delay-3s">have</span>
+            <span class="message animate__animated animate__fadeIn animate__delay-4s">many Smiles!</span>
+          </h3>
+        </div>
         <div style="margin-top:310px">
         @guest
-          {!! link_to_route('guest.signup',  __('auth.GuestLogin'), [], ['class' => 'btn btn-success m-3 animate__animated animate__swing animate__delay-3s']) !!}
+          {!! link_to_route('guest.signup',  __('auth.GuestLogin'), [], ['class' => 'btn btn-success m-3 animate__animated animate__swing animate__delay-5s']) !!}
           {!! link_to_route('login', __('auth.Login'), [], ['class' => 'btn btn-primary m-3']) !!}
           {!! link_to_route('signup.get', __('messages.register'), [], ['class' => 'btn btn-secondary m-3']) !!}
         @endguest
