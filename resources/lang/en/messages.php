8a0d3490a0d0f624b6b98b75a2d12b7506109ee2
roiaki <roitoitoitoi@yahoo.co.jp>
2022-12-11 00:54:53 +0900

    TOPページのレイアウト変更

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -9,10 +9,18 @@ return [
     'sentence02' => 'Would you like to remove cognitive distortions?',
     'register' => 'Register',
     'login' => 'Login',
+
     'about-cbt-title' => 'What is cognitive behavioral therapy?',
     'about-cbt-message01' => 'In our lives, have you ever been faced with a variety of difficult situations and had trouble solving them, or had trouble changing your mind?',
     'about-cbt-message02' => 'It is also said that when people are exposed to long-term stress, they tend to fall into extreme thinking. This is called psychological narrowing of vision.',
     'about-cbt-message03' => 'It is a psychotherapy that aims to solve problems from a "realistic and balanced" perspective that is neither overly optimistic nor pessimistic at such times. Cognitive Processing Therapy (CPT) is also specialized for recovery from PTSD that occurs after very intense events such as war experiences.',
+
+    'cognitive_distortion_title' => 'What is cognitive distortion?',
+    'cognitive_distortion_description' => 'Cognitive distortions are unconscious and biased thoughts that occur in the thought processes we use to understand things. These distortions can cause us to have difficulty viewing things objectively from one end of the spectrum to the other. There are many different types of cognitive distortions, but some of the best known are blind spots, biases, negative thinking, and poor deterministic reasoning.',
+
+    'cognitive_distortion_cause_title' => 'The Causes of Cognitive Distortions',
+    'cognitive_distortion_cause' => 'There are many possible causes of cognitive distortions. One cause of cognitive distortions is that we form our own worldview based on what we experience, hear, see, and feel. Therefore, we tend to have difficulty accepting information, opinions, and experiences from others that contradict the worldview we already have. We may also incorporate our own emotions, preferences, and biases into the thought processes we use to understand things. Thus, our emotional judgments and prejudices may prevent us from seeing things objectively. Furthermore, because we have finite knowledge and skills of our own, we may be influenced by lack of knowledge or missing information when we understand things. These can lead to cognitive distortions.',
+
     'about-thisapp-title' => 'About this app',
     'about-thisapp-message' => 'This application allows you to write down your own thoughts and feelings following the cognitive-behavioral therapy process (3-column or 7-column method). If they are difficult, they will be changed to favorable ones toward a solution. This is intended to assist in such problem solving. This application is only intended to assist. If you are not feeling well or have a serious condition, please consult a trusted psychologist or medical institution.',
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,62 @@
 @extends('layouts.app')
 
 @section('content1')
-<!-- <div class="glasscard" style="background-color:#f7f7f7; background-size:auto;"> -->
-    <div class="welcomeglasscard">
-      <div class="text-center">
-        <p class="pt-5">{{ __('messages.sentence01') }}<br>{{ __('messages.sentence02') }}</p>
-        <h1 class="outline">{{ __('messages.welcome_title') }}</h1>
-        
-        <div>
-          <h3>
-            <span class="message animate__animated animate__backInDown animate__delay-1s">May you have many smailes!</span>
-          </h3>         
-        </div>
+<section class="first">
+  <div class="welcomeglasscard">
+    <div class="text-center">
+      <p class="pt-5">{{ __('messages.sentence01') }}<br>{{ __('messages.sentence02') }}</p>
+      <h1 class="outline">{{ __('messages.welcome_title') }}</h1>
+      
+      <div>
+        <h3>
+          <span class="message animate__animated animate__backInDown animate__delay-1s">May you have many smailes!</span>
+        </h3>         
       </div>
     </div>
-    <div class="">
-      
+  </div>
+   
+  @guest
+  <div class="container text-center" style="margin-top:100px">
+    <div class="col">
+      <div class="col-sm">{!! link_to_route('guest.signup',  __('auth.GuestLogin'), [], ['class' => 'btn btn-success m-3']) !!}</div>
+      <div class="col-sm">{!! link_to_route('login', __('auth.Login'), [], ['class' => 'btn btn-primary m-3']) !!}</div>
+      <div class="col-sm">{!! link_to_route('signup.get', __('messages.register'), [], ['class' => 'btn btn-secondary m-3']) !!}</div>
     </div>
-    @guest
-        <div class="container text-center" style="margin-top:100px">
-          <div class="col">
-            <div class="col-sm">{!! link_to_route('guest.signup',  __('auth.GuestLogin'), [], ['class' => 'btn btn-success m-3']) !!}</div>
-            <div class="col-sm">{!! link_to_route('login', __('auth.Login'), [], ['class' => 'btn btn-primary m-3']) !!}</div>
-            <div class="col-sm">{!! link_to_route('signup.get', __('messages.register'), [], ['class' => 'btn btn-secondary m-3']) !!}</div>
-          </div>
-        </div>
-    @endguest
-    <div>
-      <!--<audio controlslist="nodownload" controls autoplay src="images/bgm.mp3" loop></audio>-->
+  </div>
+  @endguest
+</section>
+
+  <div>
+    <!--<audio controlslist="nodownload" controls autoplay src="images/bgm.mp3" loop></audio>-->
+  </div>
+  
+   
+  <section class="back01">
+    <div class="top_page_card">
+      <h2 class="top_page_title">{{ __('messages.about-cbt-title') }}</h2>
+      <p class="about-text">{{ __('messages.about-cbt-message01') }}</p>
+      <p class="about-text">{{ __('messages.about-cbt-message02') }}</p>
+      <p class="about-text">{{ __('messages.about-cbt-message03') }}</p>
     </div>
-    <div class="container">
-      <div class="">
-        <section class="cbt-title">
-          <h2 class="about-cbt mt-5">{{ __('messages.about-cbt-title') }}</h2>
-          <p class="about-text">{{ __('messages.about-cbt-message01') }}</p>
-          <p class="about-text">{{ __('messages.about-cbt-message02') }}</p>
-          <p class="about-text">{{ __('messages.about-cbt-message03') }}</p>
-        </sectio>
-      
-        <section class="cbt-title">
-          <h2 class="about-cbt">{{ __('messages.about-thisapp-title') }}</h2>
-          <p class="about-text">{{ __('messages.about-thisapp-message') }}</p>
-        </section>
-      </div>
+  </section>
+
+  <section class="back02">
+    <div class="top_page_card">
+      <h2 class="top_page_title">{{ __('messages.cognitive_distortion_title') }}</h2>
+      <p class="about-text">{{ __('messages.cognitive_distortion_description') }}</p>
     </div>
+    <div class="top_page_card">
+      <h2 class="top_page_title">{{ __('messages.cognitive_distortion_cause_title') }}</h2>
+      <p class="about-text">{{ __('messages.cognitive_distortion_cause') }}</p>
+    </div>
+  </section>
     
+  <section class="back01">
+    <div class="top_page_card">
+      <h2 class="top_page_title">{{ __('messages.about-thisapp-title') }}</h2>
+      <p class="about-text">{{ __('messages.about-thisapp-message') }}</p>
+    </div>
+  </section>
+  
+  
 @endsection
