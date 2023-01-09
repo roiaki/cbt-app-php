cad6a90b227df58df358cb33409454cba65b9112
roiaki <roitoitoitoi@yahoo.co.jp>
2023-01-09 22:53:50 +0900

    [fix] 解決策の削除機能の修正

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -25,6 +25,7 @@
 
     <!-- 出来事一覧カード -->
     @foreach($events as $event)
+      @if(Auth::id() === $event->user_id)
       <div class="d-flex justify-content-center">
         <div class="event_page_card col-12">
           <div class="card-body d-flex flex-row">
@@ -33,36 +34,33 @@
             </a>
             <div>
               <div class="font-weight-bold">
-                
-                
+                {{ $event->user->name }}
               </div>
               <div class="font-weight-lighter">{{ date( 'Y n/j H:i', strtotime($event->updated_at) ) }}</div>
             </div>
-      
-            @if( Auth::id() === $event->user_id )
-              <!-- dropdown -->
-              <div class="ml-auto card-text">
-                <div class="dropdown">
-                  <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-edit"></i>
+
+            <!-- dropdown -->
+            <div class="ml-auto card-text">
+              <div class="dropdown">
+                <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fas fa-edit"></i>
+                </a>
+                
+                <div class="dropdown-menu dropdown-menu-right">
+                  <a class="dropdown-item" href="{{ route('events.edit', ['event' => $event->id]) }}">
+                    <i class="fas fa-pen mr-1"></i>出来事を更新する
                   </a>
-                  
-                  <div class="dropdown-menu dropdown-menu-right">
-                    <a class="dropdown-item" href="{{ route('events.edit', ['event' => $event->id]) }}">
-                      <i class="fas fa-pen mr-1"></i>出来事を更新する
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item text-danger" 
-                      data-toggle="modal" 
-                      data-target="#modal-delete-{{$event->id}}">
-                      <i class="fas fa-trash-alt mr-1"></i>出来事を削除する
-                    </a>
-                  </div>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item text-danger" 
+                    data-toggle="modal" 
+                    data-target="#modal-delete-{{$event->id}}">
+                    <i class="fas fa-trash-alt mr-1"></i>出来事を削除する
+                  </a>
                 </div>
               </div>
-              <!-- dropdown -->
+            </div>
+            <!-- dropdown -->
 
-            @endif
           </div>
 
           <a class="text-dark" href="{{ route('events.show', $event->id) }}">
@@ -83,33 +81,36 @@
       </div>
 
       <!-- modal ok -->
-      <div style="z-index: 1500" 
-           class="modal fade" 
-           id="modal-delete-{{$event->id}}" 
-           tabindex="-1" role="dialog" 
-           aria-labelledby="basicModal" 
-           aria-hidden="true">
-        <form role="form" class="form-inline" method="post" action="{{ route('events.destroy', ['event' => $event->id] ) }}">
-          @csrf
-          @method('DELETE')
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h4 class="modal-title" id="myModalLabel">削除確認</h4>
-              </div>
-              <div class="modal-body">
-                <p></p>
-                {{ $event->created_at}}<br><br>
-                {{ $event->title }} を削除しますがいいですか？
-              </div>
-              <div class="modal-footer">
-                <a class="btn btn-light" data-dismiss="modal">閉じる</a>
-                <button type="submit" class="btn btn-danger">削除</button>
-              </div>
+      <div class="modal fade" 
+           id="modal-delete-{{ $event->id }}" 
+           tabindex="-1" 
+           role="dialog">
+        
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title" id="myModalLabel">削除確認</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+                <span aria-hidden="true">&times;</span>
+              </button>
             </div>
+            <form method="post" action="{{ route('events.destroy', ['event' => $event->id] ) }}">
+              @csrf
+              @method('DELETE')
+              <div class="modal-body">
+                <p>{{ $event->created_at}}</p>
+                <p>{{ $event->title }} </p>
+                を削除しますがいいですか？
+              </div>
+              <div class="modal-footer justify-content-between">
+                <a class="btn btn-info" data-dismiss="modal">キャンセル</a>
+                <button type="submit" class="btn btn-danger">削除する</button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>  
+        </div>
+      </div>
+      @endif
     @endforeach
     <!-- 出来事カード -->
 
@@ -121,28 +122,6 @@
       @endif
     </div>
   </div>
-</div>
-
-<!-- modal1 -->
-<div style="z-index: 1500" class="modal fade" id="SampleModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
-  //form-inline:文字の量に合わせてモーダルの大きさが変化する
-  <form role="form" class="form-inline" method="post" action="">
-  @csrf
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title" id="myModalLabel">削除確認r</h4>
-        </div>
-        <div class="modal-body">
-          <p></p>
-        </div>
-        <div class="modal-footer">
-          <a class="btn btn-light" data-dismiss="modal">閉じる</a>
-          <button type="submit" class="btn btn-danger">削除</button>
-        </div>
-      </div>
-    </div>
-  </form>
 </div>
 
 @endsection
